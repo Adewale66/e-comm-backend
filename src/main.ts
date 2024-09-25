2bf656ffefa1015f690f31fc79cb1020628b4acd
Adewale Kujore <adewalekujore6@gmail.com>
2024-09-25 15:25:56 +0100

    chore: minor changes

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SeederService } from './products/seeder.service';
 import * as cookieParser from 'cookie-parser';
-import { raw } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { rawBody: true });
@@ -12,7 +11,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1');
   app.use(cookieParser());
-  app.use('/api/v1/payment/webhook', raw({ type: 'applicaion/json' }));
   app.enableCors({
     credentials: true,
     origin: process.env.BASE_URL,
